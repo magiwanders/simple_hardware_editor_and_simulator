2fede82588a2d5e9269758fcbc49b6e2058323c6
magiwanders <magi.wanders@gmail.com>
2022-08-25 10:41:01 +0200

    Refactoring before callback refactoring

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-console.log("Simple Hardware Editor!")
-
-///////////////////////////////////////////////////
-var url = new URLSearchParams(window.location.search)
-var chip = null
-if (LZString.decompressFromBase64(url.get('chip'))==null || LZString.decompressFromBase64(url.get('chip'))=='') {
-  chip = get_empty_chip() 
-} else {
-  chip = JSON.parse(LZString.decompressFromBase64(url.get('chip')))
-}
-load(chip)
-///////////////////////////////////////////////////
-
-function count(device_name) {
-  var n = 0
-  for (var key in chip['devices']) {
-    if (chip['devices'][key].type == device_name || chip['devices'][key].celltype == device_name) {
-      n+=1
-    }
+//#region SETUP
+// This contains the code that loads the tool into existance at page load.
+console.log("S.H.E.A.S. -> Simple Hardware Editor And Simulator!")
+var url, chip, circuit, monitor, monitorview, iopanel, paper;
+document.addEventListener('DOMContentLoaded', () => {
+  url = new URLSearchParams(window.location.search)
+  // Check if the URL actually contains some chip to load, if not load empty chip
+  if (LZString.decompressFromBase64(url.get('chip'))==null || LZString.decompressFromBase64(url.get('chip'))=='') {
+    load(get_empty_chip(), false)
+  } else {
+    load(JSON.parse(LZString.decompressFromBase64(url.get('chip'))), false)
   }
-  return n
-}
+  update_url() 
+  // setInterval(update_url, 5000);
+  window.onbeforeunload = update_url
+})
+//#endregion
+
+//#region CHIPS
 
 function get_empty_chip() {
   return {
@@ -48,7 +45,6 @@ function get_output_chip() {
     type: "Lamp", 
     label: "out_"+count('Lamp'), 
     net: "out_"+count('Lamp'), 
-    net: "out", 
     order: 0, 
     bits: 1,
     position: {
@@ -71,6 +67,7 @@ function get_dff_chip() {
   return {
     type: "Dff",
     label: "dff_"+count('Dff'),
+    net: "dff_"+count('Dff'),
     bits:1,
     polarity: {
       clock: true,
@@ -83,8 +80,140 @@ function get_clock_chip() {
   return {
     type: "Clock",
     label: "clock_"+count('Clock'),
+    net: "clock_"+count('Clock'),
     bits:1
   }
+}
+
+//#endregion
+
+//#region PAGE-WIDE FUNCTIONS
+
+document.getElementById('reset').onclick = reset
+document.getElementById('reload').onclick = reload
+document.getElementById('save').onclick = save
+document.getElementById('share').onclick = share
+
+function reset() {
+  console.log('Canvas reset...')
+  load(get_empty_chip())
+}
+
+function reload() {
+  console.log('Canvas reloaded')
+  load(circuit.toJSON())
+}
+
+function load(chip_to_load, reload=true) {
+  console.log('LOAD')
+  console.log(chip_to_load)
+  chip = chip_to_load
+
+  // This happens when loading a component into the canvas not as blackbox but showing internals
+  if (chip_to_load['devices']==null) {
+    var component = chip_to_load
+    chip = get_empty_chip()
+    chip['devices']['dev0'] = component
+  }
+
+  // Reinstantiate circuit
+  delete(circuit)
+  delete(paper)
+  delete(monitor)
+  delete(monitorview)
+  circuit = new digitaljs.Circuit(chip);
+  monitor = new digitaljs.Monitor(circuit);
+  monitorview = new digitaljs.MonitorView({model: monitor, el: $('#monitor') });
+  iopanel = new digitaljs.IOPanelView({model: circuit, el: $('#iopanel') });
+  paper = circuit.displayOn($('#paper'));
+
+  // Reinstate simulation
+  if (document.getElementById('toggle_simulation').innerHTML=='Play') {
+    circuit.stop();
+  } else {
+    circuit.start();
+  }
+  monitorview.live = true
+  monitorview.listenTo(this.model, 'add', () => {console.log('Wire added to monitor.')});
+  document.getElementById('paper').setAttribute('pointer-events', 'all')
+  document.getElementById('paper').setAttribute('pointer-events', 'painted')
+
+  // Reload
+  
+  if (reload) {
+    update_url()
+    location.reload()
+  }
+}
+
+function save() {
+  var filename = 'chip.json'
+  var textInput = JSON.stringify(circuit.toJSON())
+  var element = document.createElement('a');
+  element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
+  element.setAttribute('download', filename);
+  document.body.appendChild(element);
+  element.click();
+}
+
+function share() {
+  update_url()
+  if (url.get('chip').length >= 30000) {
+    alert('Circuit too big to be shared.')
+  }
+}
+
+function update_url() {
+  set_url(LZString.compressToBase64(JSON.stringify(circuit.toJSON()))) 
+}
+
+function set_url(new_url) {
+  url.set("chip", new_url)
+  var nextURL = '?' + url.toString()
+  var nextTitle = document.title;
+  var nextState = { additionalInformation: 'Updated the URL with JS' };
+  window.history.replaceState(nextState, nextTitle, nextURL);
+}
+
+//#endregion
+
+//#region SIMULATION FUNCTIONS
+
+document.getElementById('add').onclick = function(){
+  selected_chip(add)
+}
+document.getElementById('load').onclick = function(){
+  selected_chip(load)
+}
+document.getElementById('remove').onclick = remove_chip
+document.getElementById('rename').onclick = rename_chip
+document.getElementById('toggle_simulation').onclick = toggle_simulation
+document.getElementById('step').onclick = step
+// Handlers for zoom of signal monitors
+document.getElementById('ppt_up').onclick = (e) => { monitorview.pixelsPerTick *= 2;}
+document.getElementById('ppt_down').onclick = (e) => { monitorview.pixelsPerTick /= 2; }
+
+// Counts number of currently displayed devices
+function count(device_name) {
+  var n = 0
+  for (var key in chip['devices']) {
+    if (chip['devices'][key].type == device_name || chip['devices'][key].celltype == device_name) {
+      n+=1
+    }
+  }
+  return n
+}
+
+// Check there is not that very same name in the list of current chips
+function name_already_present(name) {
+  console.log('Checking name...' + name)
+  for (var key in chip['devices']) {
+    if (chip['devices'][key].label == name ) {
+      alert('Chip with same name ('+ name + ') already present.')
+      return true
+    } 
+  }
+  return false
 }
 
 function saved_chip(callback) {
@@ -92,6 +221,8 @@ function saved_chip(callback) {
   input.click();
 }
 
+// Callback can be either add or load the chip selected in the curtain input by the user.
+// If a saved chip is requested, the callback gets passed to the saved chip selector.
 function selected_chip(callback) {
   var selected = document.getElementById('components').value
   switch (selected) {
@@ -103,22 +234,6 @@ function selected_chip(callback) {
     case "saved_circuit": saved_chip(callback); break;
     default: console.log("FUNCTION NOT YET IMPLEMENTED"); break;
   }
-}
-
-function reset() {
-  console.log('Canvas reset')
-  chip = get_empty_chip()
-  load(chip)
-  set_url(JSON.stringify(get_empty_chip()))
-  // console.log(chip)
-}
-
-function reload() {
-  console.log('Canvas reloaded')
-  chip = circuit.toJSON()
-  load(chip)
-  update_url()
-  location.reload()
 }
 
 function toggle_simulation() {
@@ -135,148 +250,80 @@ function step() {
   circuit.updateGates();
 }
 
-function save() {
-  if (count('Clock')<0) {
-    alert('Clocks are only for live demonstration, cannot save a circuit with clock!')
-  } else {
-    var filename = 'chip.json'
-    var textInput = JSON.stringify(circuit.toJSON())
-    var element = document.createElement('a');
-    element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
-    element.setAttribute('download', filename);
-    document.body.appendChild(element);
-    element.click();
-  }
-}
-
-function remove() {
-  chip = circuit.toJSON()
+function remove_chip() {
+  var new_chip = circuit.toJSON()
   var to_remove = document.getElementById("to_remove").value
   var key_to_remove = undefined
-  for (var key in chip['devices']) {
-    if (chip['devices'][key].label == to_remove) {
+  for (var key in new_chip['devices']) {
+    if (new_chip['devices'][key].label == to_remove) {
       key_to_remove = key
-      delete chip['devices'][key]
+      delete new_chip['devices'][key]
     }
   }
 
   var i = 0
-  var keys = chip['devices']
-  chip['devices'] = {}
+  var keys = new_chip['devices']
+  new_chip['devices'] = {}
   for (var old_key in keys) {
     i += 1
     var new_key = 'dev' + i 
-    chip['devices'][new_key] = keys[old_key]
-    for (var connection in chip['connectors']) {
-      if(chip['connectors'][connection]['from'].id == old_key) {
-        chip['connectors'][connection]['from'].id = new_key
+    new_chip['devices'][new_key] = keys[old_key]
+    for (var connection in new_chip['connectors']) {
+      if(new_chip['connectors'][connection]['from'].id == old_key) {
+        new_chip['connectors'][connection]['from'].id = new_key
       }
-      if(chip['connectors'][connection]['to'].id == old_key) {
-        chip['connectors'][connection]['to'].id = new_key
+      if(new_chip['connectors'][connection]['to'].id == old_key) {
+        new_chip['connectors'][connection]['to'].id = new_key
       }
     }
   }
 
-  for (var connection in chip['connectors']) {
-    if (chip['connectors'][connection].from.id == key_to_remove || chip['connectors'][connection].to.id == key_to_remove) {
-      delete chip['connectors'][connection]
+  for (var connection in new_chip['connectors']) {
+    if (new_chip['connectors'][connection].from.id == key_to_remove || new_chip['connectors'][connection].to.id == key_to_remove) {
+      delete new_chip['connectors'][connection]
     }
   }
 
   var i = 0
-  var connections = chip['connectors']
-  chip['connectors'] = []
+  var connections = new_chip['connectors']
+  new_chip['connectors'] = []
   for (var connection in connections) {
-    chip['connectors'][i] = connections[connection]
+    new_chip['connectors'][i] = connections[connection]
     i += 1
   }
-  load(chip)
+  load(new_chip)
 }
 
-function rename() {
-  chip = circuit.toJSON()
+function rename_chip() {
+  var new_chip = circuit.toJSON()
   var to_rename = document.getElementById("to_rename").value
   var new_name = document.getElementById("new_name").value
-  for (var key in chip['devices']) {
-    if (chip['devices'][key].label == new_name ) {
-      return
+  if (name_already_present(new_name)) return
+  for (var key in new_chip['devices']) {
+    if (new_chip['devices'][key].label == to_rename ) {
+      new_chip['devices'][key].label = new_name
+      new_chip['devices'][key].net = new_name
     } 
   }
-  for (var key in chip['devices']) {
-    if (chip['devices'][key].label == to_rename ) {
-      chip['devices'][key].label = new_name
-      chip['devices'][key].net = new_name
-    } 
-  }
-    load(chip)
+    load(new_chip)
 }
 
-function load(chip) {
-  console.log('LOAD')
-  console.log(chip)
-
-  // This happens when loading a component into the canvas not as blackbox but showing internals
-  if (chip['devices']==null) {
-    var component = chip
-    chip = get_empty_chip()
-    chip['devices']['dev0'] = component
-  }
-
-  //
-  delete(circuit)
-  delete(monitor)
-  delete(monitorview)
-  circuit = new digitaljs.Circuit(chip);
-  monitor = new digitaljs.Monitor(circuit);
-  monitorview = new digitaljs.MonitorView({model: monitor, el: $('#monitor') });
-  iopanel = new digitaljs.IOPanelView({model: circuit, el: $('#iopanel') });
-
-  circuit.displayOn($('#paper'));
-  if (document.getElementById('toggle_simulation').innerHTML=='Play') {
-    circuit.stop();
-  } else {
-    circuit.start();
-  }
-  monitorview.live = true
-  monitorview.listenTo(this.model, 'add', () => {console.log('aaaaded')});
-  update_url()
-}
-
-function share() {
-  update_url()
-  if (url.get('chip').length >= 30000) {
-    alert('Circuit too big to be shared.')
-  }
-}
-
-function update_url() {
-  document.getElementById('paper').setAttribute('pointer-events', 'all')
-  document.getElementById('paper').setAttribute('pointer-events', 'painted')
-  set_url(LZString.compressToBase64(JSON.stringify(circuit.toJSON()))) 
-}
-
-function set_url(new_url) {
-  url.set("chip", new_url)
-  var nextURL = '?' + url.toString()
-  var nextTitle = document.title;
-  var nextState = { additionalInformation: 'Updated the URL with JS' };
-  window.history.replaceState(nextState, nextTitle, nextURL);
-}
-
+// Add either component or saved subcircuit to simulation
 function add(to_add, is_subcircuit, subcircuit_type) {
-  chip = circuit.toJSON()
-  var n = Object.keys(chip['devices']).length + 1
+  var new_chip = circuit.toJSON() // Save current chip
+  var n = Object.keys(new_chip['devices']).length + 1 
+  if (name_already_present(to_add.label)) return
   if (is_subcircuit) {
-    chip['devices']['dev' + n] = {
+    new_chip['devices']['dev' + n] = {
       type: "Subcircuit",
       label: subcircuit_type + '_' + count(subcircuit_type),
       celltype: subcircuit_type,
     }
-    chip['subcircuits'] = subcircuitfy(to_add, subcircuit_type)
+    new_chip['subcircuits'] = subcircuitfy(to_add, subcircuit_type)
   } else {
-    chip['devices']['dev' + n] = to_add
+    new_chip['devices']['dev' + n] = to_add
   }
-  load(chip)
+  load(new_chip)
 }
 
 // Make a chip added as subcircuit into subcircuit form.
@@ -298,28 +345,14 @@ function subcircuitfy(subcircuit, subcircuit_type) {
   return to_return
 }
 
+//#endregion
+
+// DEBUG FUNCTION (utility)
+document.getElementById('debug').setAttribute('visible', false)
+document.getElementById('debug').onclick = debug
 function debug() {
   console.log(monitor.getWiresDesc())
 }
-
-document.getElementById('add').onclick = function(){
-  selected_chip(add)
-  
-}
-document.getElementById('load').onclick = function(){
-  selected_chip(load)
-}
-document.getElementById('reset').onclick = reset
-document.getElementById('reload').onclick = reload
-document.getElementById('save').onclick = save
-document.getElementById('remove').onclick = remove
-document.getElementById('rename').onclick = rename
-document.getElementById('share').onclick = share
-document.getElementById('toggle_simulation').onclick = toggle_simulation
-document.getElementById('step').onclick = step
-document.getElementById('debug').onclick = debug
-document.getElementById('ppt_up').onclick = (e) => { monitorview.pixelsPerTick *= 2;}
-document.getElementById('ppt_down').onclick = (e) => { monitorview.pixelsPerTick /= 2; }
 
 var global_callback = undefined
 var last_read_file = undefined
@@ -336,7 +369,3 @@ input.onchange = e => {
   }
   e.target.value = null
 }
- 
-// setInterval(update_url, 5000);
-
-window.onbeforeunload = update_url
