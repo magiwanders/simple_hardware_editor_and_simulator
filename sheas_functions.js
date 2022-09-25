fe1b8546f20d8a78e8fab4d042f21e00d673595c
magiwanders <magi.wanders@gmail.com>
2022-09-25 13:27:22 +0200

    Fix

--- sheas_functions.js
+++ sheas_functions.js
@@ -2,6 +2,26 @@ var url, chip, circuit, monitor, monitorview, iopanel, paper;
 
 //#region PAGE-WIDE FUNCTIONS
 
+function setup() {
+  url = new URLSearchParams(window.location.search)
+  saved_chip_state = localStorage.getItem('chip')
+  // Check if the URL actually contains some chip to load, if not check for chip saved in local memory, if empty load empty chip
+  if (url.get('chip')==null || url.get('chip')=='') {
+    if (saved_chip_state==null || saved_chip_state=='') {
+      load(get_empty_chip(), false)
+    } else {
+      load(JSON.parse(LZString.decompressFromBase64(saved_chip_state)), false)
+    }
+  } else {
+    load(JSON.parse(LZString.decompressFromBase64(url.get('chip'))), false)
+    set_url('chip', '')
+  }
+  document.getElementById('components').value = url.get('select')
+  display_additional_settings()
+  if (url.get('bits')!=undefined && url.get('bits')!=null && url.get('bits')!='') document.getElementById('bits').value = url.get('bits')
+  save_state()
+}
+
   function shutdown() {
     save_state()
     monitorview.shutdown()
